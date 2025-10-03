541ef9c95e13a6dcdb91c763bc592dcccb6c716e
wtlwang2019 <53394996+wtlwang2019@users.noreply.github.com>
2025-10-03 22:08:58 +0800

    Update run.sh

--- test/run.sh
+++ test/run.sh
@@ -6,4 +6,4 @@ ls -l /mnt
 
 cat /start.sh || echo 0
 
-./llama-cli -m  qwen2.5-0.5b-instruct-q4_k_m.gguf  -p "tell a joke" 
+./llama-cli -m  qwen2.5-0.5b-instruct-q4_k_m.gguf  -p "tell a joke" || echo 0
